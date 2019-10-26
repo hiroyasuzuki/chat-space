ea1a498ca405d3593849b8d75822f79e3b43f5a5
YourName <you@example.com>
2019-10-26 22:49:21 +0900

    作成したグループをサイドバーへ表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'meseeages#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, onry: [:new, :create, :edit, :update]
+  resources :groups, onry: [:index, :new, :create, :edit, :update]
 end
